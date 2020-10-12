2f25652aa2f998c739a4e287ce47bad65b18593d
Zygimantas Daunoras <daunoraszygimantas@gmail.com>
2020-10-12 20:18:01 +0000

    Made the ball rolling

--- index.java
+++ index.java
@@ -2,6 +2,9 @@ void setup() {
 	size(500, 500);
 }
 
+int x = 0;
+int y = 0;
+
 void draw() {
 // Step 1: Write code that draws the following screenshots 
 // with hard-coded values. (Feel free to use colors 
@@ -15,11 +18,14 @@ void draw() {
 // For example, “variable1 = variable1 + 2;”. 
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
+	int black = 0, gray = 175, white = 255;
+	int height = 50, width = 50;
+  	background(gray);
+ 	stroke(black);
 
-  background(255);
-  stroke(0);
+	x+=1;
+	y+=1;
 
-
-  
-
+	fill(white);
+	ellipse(x, y, 50, 50);
 }
